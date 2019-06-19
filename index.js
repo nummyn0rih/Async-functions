ab457aaa783bdf23de3882e77f4575e37f46de8f
Ivan Malshchukov <nummyn0rih@gmail.com>
2019-06-19 15:35:31 +0300

    complete asinc func

--- index.js
+++ index.js
@@ -3,5 +3,23 @@
  * @param {Function} callback
  */
 module.exports = function (operations, callback) {
-
+    let hasError;
+    let result = [];
+    
+    operations.forEach(function(fn, index) {
+        fn(function(err, data) {
+            if (hasError) {
+                return;
+            } else if (err) {
+                callback(err);
+                hasError = true;
+            } else {
+                //console.log(arguments, index);
+                result[index] = data;
+                if (result.length === operations.length && !result.includes(undefined)) {
+                    callback(null, result);
+                }
+            }
+        })
+    })
 };
